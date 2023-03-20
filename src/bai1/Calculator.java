ba5993d51d52f6893e1dc58b23225ea192bf0c3b
quangtuan9997 <mrt.tuan97@gmail.com>
2023-03-20 08:56:47 +0700

    first commit

---
+++ src/bai1/Calculator.java
@@ -0,0 +1,12 @@
+package bai1;
+
+public class Calculator {
+    public static int add(int first,int second){
+//        throw new UnsupportedOperationException();
+   return first + second;
+    }
+    public static int sub(int first,int second){
+//        throw new UnsupportedOperationException();
+         return first - second;
+    }
+}
---
+++ src/bai2/AbsoluteNumberCalculator.java
@@ -0,0 +1,10 @@
+package bai2;
+
+public class AbsoluteNumberCalculator {
+    public static int findAbsolute(int numbers){
+        if (numbers<0){
+            return -numbers;
+        }
+        return numbers;
+    }
+}
